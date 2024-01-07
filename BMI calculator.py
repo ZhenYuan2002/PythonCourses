b5c6e95d2f76b06fdbda6a5d8e901d2387508865
ZhenYuan2002 <152847268+ZhenYuan2002@users.noreply.github.com>
2024-01-07 18:20:35 +0800

    BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -28,3 +28,18 @@ Enter your weight in kg
 Your BMI is 25.88, which puts you in the slightly overweight category
 
 
+#Alternatively, there are also elif and else statements used to save memory space
+height = float(input())  # in meters e.g., 1.55
+weight = int(input())  # in kilograms e.g., 72
+# Your code below this line 👇
+bmi = weight / (height * height)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
