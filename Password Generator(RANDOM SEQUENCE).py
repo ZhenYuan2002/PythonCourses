4ce2109785165dde47c463d62d77d4d3348cbac6
Zhen Yuan <152847268+ZhenYuan2002@users.noreply.github.com>
2024-01-12 16:08:13 +0800

    Password Generator(RANDOM SEQUENCE).py

--- Password Generator(RANDOM SEQUENCE).py
+++ Password Generator(RANDOM SEQUENCE).py
@@ -34,6 +34,54 @@ print(f"Your password is: {password}")
 
 
 
+#Advanced passwordd generator with exceptions
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+# Function to get a valid number from the user
+def get_valid_number(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value > 0:
+                return value
+            else:
+                print("Please enter a number greater than 0.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+nr_letters = get_valid_number("How many letters would you like in your password?\n")
+nr_symbols = get_valid_number(f"How many symbols would you like?\n")
+nr_numbers = get_valid_number(f"How many numbers would you like?\n")
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
+
+
+
 
 
 
