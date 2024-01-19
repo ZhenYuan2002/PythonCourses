bd3d6688a39db0f91778d91a1c124b5cbe23f4eb
Zhen Yuan <152847268+ZhenYuan2002@users.noreply.github.com>
2024-01-19 15:37:35 +0800

    Advanced Calclulator.py

--- Advanced Calclulator.py
+++ Advanced Calclulator.py
@@ -1,3 +1,8 @@
+#!!!!!!!!UPDATE
+#Code now no longer works in python, as function when returned is sotred in memory
+
+
+
 #MY TAKE
 
 #Add
@@ -48,48 +53,49 @@ print(f'{num1} {operation_symbol} {num2} = {answer}')
 
 
 #Course Answer
-#Add
+#Done on replit
+from replit import clear
+from art import logo
+
 def add(n1, n2):
-    return float(n1 + n2)
+  return n1 + n2
 
-#Subtract
 def subtract(n1, n2):
-    return float(n1 - n2)
+  return n1 - n2
 
-#Divide
-def divide(n1, n2):
-    return float(n1 / n2)
-
-#Multiply
 def multiply(n1, n2):
-    return float(n1 * n2)
+  return n1 * n2
 
+def divide(n1, n2):
+  return n1 / n2
 
 operations = {
-    "+" : add,
-    "-" : subtract,
-    "/" : divide,
-    "*" : multiply
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
-print(operations)
 
-num1 = int(input('Whats the first number?'))
-num2 = int(input('Whats the second number?'))
+def calculator():
+  print(logo)
 
-for symbol in operations.keys():
-    print(symbol, end = ' ')
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input('\nChoose a symbol from the line above')
-if operation_symbol == '+':
-    answer = add
-elif operation_symbol == '-':
-    answer = subtract
-elif operation_symbol == '/':
-    answer = divide
-elif operation_symbol == '*':
-    answer = multiply
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
 
-#calculator_function = operations[operation_symbol]
-#calculator_function(num1, num2)
-
-print(f'{num1} {operation_symbol} {num2} = {answer}')
+calculator()
